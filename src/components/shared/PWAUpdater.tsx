c6410f924fd55cc41a241d5663ec1a58b0a109b0
James Aworinde <jamesaworinde@192.168.0.104>
2026-06-17 11:36:12 +0100

    fix: prevent mobile blank page from stale cache

--- src/components/shared/PWAUpdater.tsx
+++ src/components/shared/PWAUpdater.tsx
@@ -7,23 +7,14 @@ export default function PWAUpdater() {
     if (typeof window === 'undefined') return
     if (!('serviceWorker' in navigator)) return
 
-    let reloaded = false
-    const onControllerChange = () => {
-      if (reloaded) return
-      reloaded = true
-      window.location.reload()
-    }
-    navigator.serviceWorker.addEventListener('controllerchange', onControllerChange)
+    navigator.serviceWorker.getRegistrations()
+      .then((registrations) => Promise.all(registrations.map((registration) => registration.unregister())))
+      .catch(() => {})
 
-    navigator.serviceWorker.getRegistration().then((reg) => {
-      if (!reg) return
-      reg.update().catch(() => {})
-      const interval = setInterval(() => reg.update().catch(() => {}), 60 * 60 * 1000)
-      return () => clearInterval(interval)
-    })
-
-    return () => {
-      navigator.serviceWorker.removeEventListener('controllerchange', onControllerChange)
+    if ('caches' in window) {
+      window.caches.keys()
+        .then((keys) => Promise.all(keys.map((key) => window.caches.delete(key))))
+        .catch(() => {})
     }
   }, [])
 
